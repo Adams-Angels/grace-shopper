0e68ef179617754aaf1587f775bc7b442429be83
Adam Siegel <siegelini@icloud.com>
2023-06-19 12:31:27 -0400

    added getUserByUsername

--- db/adapters/users.js
+++ db/adapters/users.js
@@ -59,6 +59,22 @@ async function getUserById(userId) {
   }
 }
 
+async function getUserByUsername(username) {
+  try {
+    const {
+      rows: [user],
+    } = await client.query(
+      `
+      SELECT * FROM users WHERE username = $1;
+      `,
+      [username]
+    );
+    return user;
+  } catch (error) {
+    throw error;
+  }
+}
+
 async function getAllUsers() {
   const { rows } = await client.query(`
     SELECT * FROM users;
@@ -70,5 +86,6 @@ module.exports = {
   createUser,
   getUser,
   getUserById,
+  getUserByUsername,
   getAllUsers,
 };
