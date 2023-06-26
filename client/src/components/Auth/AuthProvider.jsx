3248ad587cd1307c30bae286d44388e27d0782b2
Adam Siegel <siegelini@icloud.com>
2023-06-26 11:50:23 -0400

    added authProvider, wrapped app

---
+++ client/src/components/Auth/AuthProvider.jsx
@@ -0,0 +1,39 @@
+import { createContext, useState, useEffect } from "react";
+import { fetchMe } from "../../api/auth";
+export const AuthContext = createContext();
+
+const AuthProvider = ({ children }) => {
+  const [user, setUser] = useState({ id: null, username: "Guest" });
+  const [loggedIn, setLoggedIn] = useState(false);
+
+  useEffect(() => {
+    async function getMe() {
+      try {
+        const { user } = await fetchMe();
+        setUser(user);
+        if (Object.keys(user).length > 0) {
+          setLoggedIn(true);
+        } else {
+          setLoggedIn(false);
+        }
+      } catch (error) {
+        setUser({ username: "Guest" });
+        setLoggedIn(false);
+      }
+    }
+    getMe();
+  }, []);
+
+  const contextValue = {
+    user,
+    setUser,
+    loggedIn,
+    setLoggedIn,
+  };
+
+  return (
+    <AuthContext.Provider value={contextValue}>{children}</AuthContext.Provider>
+  );
+};
+
+export default AuthProvider;
