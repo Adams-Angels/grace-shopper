f1582bde042c60e3191033791983ef89db99c0a1
LindsayBrumley <lalainaphillips@gmail.com>
2023-06-28 15:13:59 -0400

    displayiny product details and price

--- client/src/components/Auth/AuthProvider.jsx
+++ client/src/components/Auth/AuthProvider.jsx
@@ -1,5 +1,5 @@
 import { createContext, useState, useEffect } from "react";
-import { fetchMe } from "../../api/auth";
+import {} from "../../api/auth";
 export const AuthContext = createContext();
 
 const AuthProvider = ({ children }) => {
