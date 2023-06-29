062ff04cc6b7effc0e97f3d898526e79536b05f0
Vanessaines2 <Vanessaines2@icloud.com>
2023-06-29 10:58:28 -0400

    auth products, edit and delete api

--- client/src/api/products.js
+++ client/src/api/products.js
@@ -8,12 +8,91 @@ export async function fetchAllProducts() {
     console.error(error);
   }
 }
+
 export async function fetchProductById(id) {
   try {
     const response = await fetch(`/api/products/${id}`);
     const result = await response.json();
     console.log("product by id from api", result);
     return result;
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+export async function createProduct(
+  name,
+  description,
+  price,
+  image,
+  inventory,
+  category
+) {
+  try {
+    const response = await fetch(`/api/products/`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      //might need authorization or may not ?
+      body: JSON.stringify({
+        post: {
+          name,
+          description,
+          price,
+          image,
+          inventory,
+          category,
+        },
+      }),
+    });
+    const result = await response.json();
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+export async function updateProduct(
+  id,
+  name,
+  description,
+  price,
+  image,
+  inventory,
+  category
+) {
+  try {
+    const response = await fetch(`/api/products/${id}`, {
+      method: "PATCH",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        post: {
+          name,
+          description,
+          price,
+          image,
+          inventory,
+          category,
+        },
+      }),
+    });
+    const result = await response.json();
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+export async function deleteProduct(id) {
+  try {
+    const response = await fetch(`/api/products/${id}`, {
+      method: "DELETE",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    const result = await response.json();
   } catch (error) {
     console.error(error);
   }
