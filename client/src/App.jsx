f131722894397e38177b93ea950382510b629d55
rylee <ryfrogquinn@gmail.com>
2023-06-29 09:17:07 -0500

    added header and homepage

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,15 +1,23 @@
 import { useEffect, useState } from "react";
 import "./App.css";
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Link } from "react-router-dom";
 import { ProductsList } from "../src/components/ProductsList";
 import { AuthForm } from "../src/components/AuthForm";
 import { ProductItem } from "../src/components/ProductItem";
+import { Home } from "../src/components/Home";
 
 function App() {
   return (
     <div>
+      <header>
+        <h1>All Things Frog</h1>
+        <Link to="/">Home</Link>
+        <Link to="/login">Login</Link>
+        <Link to="/products">All Products</Link>
+      </header>
       <Routes>
-        <Route path="/" element={<ProductsList />} />
+        <Route path="/" element={<Home />} />
+        <Route path="/products" element={<ProductsList />} />
         <Route path="/login" element={<AuthForm />} />
         <Route path="/register" element={<AuthForm />} />
         <Route path="/products/:id" element={<ProductItem />} />
---
+++ client/src/components/Home.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+export function Home() {
+  return (
+    <div>
+      <h2>Homepage</h2>
+    </div>
+  );
+}
